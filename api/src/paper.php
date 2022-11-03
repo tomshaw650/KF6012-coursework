03ea39368157d1848adb56018b2d41e349ed3cde
Tom Shaw <tomshaw650@gmail.com>
2022-11-03 16:54:27 +0000

    added author_id paramater to paper

--- api/src/paper.php
+++ api/src/paper.php
@@ -10,6 +10,7 @@
  * 		- if params are passed, filters the results
  * 		- PARAMS: 
  * 			- track: returns papers from a specific track
+ * 			- author ID: returns all papers associated with specified author
  * @param array endpointParams - creates an array of params to be passed
  * 
  * @author Tom Shaw
@@ -20,7 +21,8 @@ class Paper extends Endpoint
 	protected function initialiseSQL() {
 		$sql = "SELECT paper.paper_id, paper.title, paper.award, paper.abstract, track.short_name AS track, track.name
                 FROM paper
-                INNER JOIN track ON (paper.track_id = track.track_id)";
+                INNER JOIN track ON (paper.track_id = track.track_id)
+				INNER JOIN paper_has_author ON (paper.paper_id = paper_has_author.paper_id)";
 		$this->setSQL($sql);
 		$sqlParams = [];
 
@@ -35,18 +37,30 @@ class Paper extends Endpoint
 			$sqlParams['track'] = '%' . $track . '%';
 		}
 
+		if (filter_has_var(INPUT_GET, 'author_id')) {
+			if (!filter_var($_GET['author_id'], FILTER_VALIDATE_INT)) {
+				http_response_code(400);
+				$output['message'] = "ID must be an integer";
+				die(json_encode($output));
+			}
+
+			if (isset($where)) {
+				$where .= " AND paper_has_author.authorId = :author_id";
+			} else {
+				$where = " WHERE paper_has_author.authorId = :author_id";
+			}
+			$sqlParams['author_id'] = $_GET['author_id'];
+		}
+
 		if (isset($where)) {
 			$sql .= $where;
 		}
 
 		$this->setSQL($sql);
 		$this->setSQLParams($sqlParams);
-
-		var_dump($sql);
-		var_dump($sqlParams);
 	}
 
 	protected function endpointParams() {
-		return ['track'];
+		return ['track', 'author_id'];
 	}
 }
