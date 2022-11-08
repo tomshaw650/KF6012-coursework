b7f8fcfc629bcdcce883ee83e0f68bb0f65aeadd
Tom Shaw <tomshaw650@gmail.com>
2022-11-08 20:03:40 +0000

    created components folder and started work on header and nav

---
+++ app/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="flex flex-col">
+      <h1 className="text-6xl font-bold">CHI Play 2021</h1>
+      <h2 className="text-2xl font-bold text-white">Where HCI Grows</h2>
+    </header>
+  );
+}
---
+++ app/src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function NavBar() {
+  const navItems = [
+    { name: "Home", link: "/" },
+    { name: "Tracks", link: "/tracks" },
+    { name: "Authors", link: "/authors" },
+  ];
+  return (
+    <nav className="flex justify-center">
+      <ul className="mt-8 flex flex-row gap-12 text-2xl">
+        {navItems.map((item) => (
+          <li key={item.name}>
+            <a className="text-white" href={item.link}>
+              {item.name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
