e614a57ab39ffb5442855731e17ad412a65fb292
Tom Shaw <tomshaw650@gmail.com>
2022-11-08 21:01:30 +0000

    landing redesign

--- app/src/App.js
+++ app/src/App.js
@@ -7,12 +7,15 @@ import Header from "./components/Header";
 export default function App() {
   return (
     <div className="h-screen w-screen bg-bgblack">
-      <span className="sr-only">CHI Play Conference 2021</span>
-      <NavBar />
-
-      <div>
+      <div className="flex justify-between">
         <Header />
+        <NavBar />
       </div>
+      <p class="text-white ">
+        Welcome to the main conference website. Here you can browse all related
+        info, including all the papers and authors at CHI Play. Use the menu at
+        the top to navigate.
+      </p>
     </div>
   );
 }
--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="flex flex-col">
-      <h1 className="text-6xl font-bold">CHI Play 2021</h1>
-      <h2 className="text-2xl font-bold text-white">Where HCI Grows</h2>
+    <header className="m-5 flex flex-col">
+      <h1 className="text-7xl font-bold">CHI Play 2021</h1>
+      <h2 className="ml-5 text-2xl font-bold text-gray-400">
+        where HCI evolves.
+      </h2>
     </header>
   );
 }
