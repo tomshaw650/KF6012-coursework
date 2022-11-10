c608f98b6ae316014894ed80b01ef34cc942b51a
Tom Shaw <tomshaw650@gmail.com>
2022-11-10 17:25:29 +0000

    pages directory + landing page added, react router refactor

--- app/src/App.js
+++ app/src/App.js
@@ -1,31 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./index.css";
 
-import NavBar from "./components/navigation/NavBar";
-import Header from "./components/Header";
-import HeroImage from "./components/HeroImage";
-import LandingContent from "./components/LandingContent";
-import Footer from "./components/Footer";
-import MobileNavBar from "./components/navigation/MobileNavBar";
+import LandingPage from "./pages/LandingPage";
 
 export default function App() {
   return (
     <div className="h-screen w-screen overflow-hidden">
-      <div className="flex justify-between bg-bgdark">
-        <Header />
-        <NavBar />
-        <MobileNavBar />
-      </div>
-      <div className="flex flex-col justify-evenly md:flex-row">
-        <LandingContent
-          title="Discover the Conference"
-          body="Welcome to the main conference website. Here you can browse all related
-        info, including all the papers and authors at CHI Play. Use the menu at
-        the top to navigate."
-        />
-        <HeroImage />
-      </div>
-      <Footer />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
     </div>
   );
 }
