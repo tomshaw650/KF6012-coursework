69897cd7edf2a778557494117e6171ca83fefd7f
Tom Shaw <tomshaw650@gmail.com>
2022-11-10 09:25:23 +0000

    footer fixed at mobile view

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -1,7 +1,7 @@
 // create footer component
 export default function Footer() {
   return (
-    <footer className="mt-8 flex flex-row justify-center gap-10 bg-bgdark py-2 md:mt-0 md:py-4">
+    <footer className="mt-16 flex flex-row justify-center gap-10 bg-bgdark py-2 md:mt-0 md:py-4">
       <p className="text-md text-center text-white md:text-2xl">
         Created by <a href="http://github.com/tomshaw650">Tom Shaw</a>
       </p>
