572f18ebe4ce33337ac5f424b833f99d59f6430b
Tom Shaw <tomshaw650@gmail.com>
2022-11-10 17:02:20 +0000

    Revert "responsive landing v2"
    This reverts commit 46695b8f41965c80e5a6603dc8298f4aca46c4de.

--- app/src/components/HeroImage.js
+++ app/src/components/HeroImage.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import hero_img1 from "../images/hero_img1.jpg";
 
-export default function HeroImage() {
+export default function HeroImage(props) {
   return (
     <>
       <img
         src={hero_img1}
-        alt="Conference audience in a dark auditorium"
-        className="h-72 w-72 rounded-full ring-4 ring-white sm:left-20 sm:mr-12 md:ml-12 md:mb-14 md:mt-16 md:flex md:h-96 md:w-96"
+        alt="hero_img1"
+        className="ml-12 h-72 w-72 rounded-full ring-4 ring-white md:mb-14 md:mt-16 md:flex md:h-96 md:w-96"
       />
     </>
   );
