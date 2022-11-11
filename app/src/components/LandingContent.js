598d2a68585592dc350645b2f11c6e2cb5f6e63e
Tom Shaw <Tomshaw650@gmail.com>
2022-11-11 22:13:56 +0000

    mobile view fix! finally

--- app/src/components/LandingContent.js
+++ app/src/components/LandingContent.js
@@ -1,8 +1,8 @@
 export default function LandingContent(props) {
   return (
     <div className="m-10 flex max-w-xl flex-col md:m-20">
-      <h2 className="text-5xl text-white md:mt-20">{props.title}</h2>
-      <p className="text-xl font-normal text-white">{props.body}</p>
+      <h2 className="text-3xl md:text-5xl text-white md:mt-20">{props.title}</h2>
+      <p className="text-md md:text-xl font-normal text-white">{props.body}</p>
     </div>
   );
 }
