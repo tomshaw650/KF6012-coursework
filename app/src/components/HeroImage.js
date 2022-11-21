9abad8f9424adb579c0bdf4ce60642bf5206eed7
Tom Shaw <tomshaw650@gmail.com>
2022-11-21 11:03:50 +0000

    image stuff!

--- app/src/components/HeroImage.js
+++ app/src/components/HeroImage.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import hero_img1 from "../images/hero_img1.jpg";
+import hero_img from "../images/hero_img.jpg";
 
 export default function HeroImage() {
   return (
     <>
       <img
-        src={hero_img1}
+        src={hero_img}
         alt="Dark Conference Audience by user Headway on website Unsplash"
         className="ml-24 h-72 w-72 rounded-full ring-4 ring-white md:mb-14 md:mt-16 md:flex md:h-96 md:w-96"
       />
